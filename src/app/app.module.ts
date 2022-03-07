d50bcf204b0c2155f64ae8ffc3909d9f0a5047b7
Steve Schaner <schanerst@gmail.com>
2022-03-07 12:01:33 -0500

    Modified core module to include routing and created tables for people and planets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PeopleModule } from './people/people.module';
-import { PeopleListComponent } from './people/people-list/people-list.component';
 import { PlanetsModule } from './planets/planets.module';
-import { PlanetsListComponent } from './planets/planets-list/planets-list.component';
-
-const routes: Routes = [
-  { path: 'people', component: PeopleListComponent },
-  { path: 'planets', component: PlanetsListComponent },
-  { path: '', redirectTo: '/people', pathMatch: 'full' },
-];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { NavigationComponent } from './navigation/navigation.component';
+import { PeopleListComponent } from '../people/people-list/people-list.component';
+import { PlanetsListComponent } from '../planets/planets-list/planets-list.component';
 
-
+const routes: Routes = [
+  { path: 'people', component: PeopleListComponent },
+  { path: 'planets', component: PlanetsListComponent },
+  { path: '', redirectTo: '/people', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [
-    NavigationComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    NavigationComponent
-  ]
+  declarations: [NavigationComponent],
+  imports: [RouterModule.forRoot(routes), CommonModule],
+  exports: [NavigationComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
