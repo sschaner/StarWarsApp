ab645871fd36a1a7242358fc41a6e31256f1d6e2
Steve Schaner <schanerst@gmail.com>
2022-03-08 09:50:03 -0500

    Navigation now works from in the core module with routing in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PeopleModule } from './people/people.module';
 import { PlanetsModule } from './planets/planets.module';
+import { PeopleListComponent } from './people/people-list/people-list.component';
+import { PlanetsListComponent } from './planets/planets-list/planets-list.component';
+
+const routes: Routes = [
+  { path: 'people', component: PeopleListComponent },
+  { path: 'planets', component: PlanetsListComponent },
+  { path: '', redirectTo: '/people', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { NavigationComponent } from './navigation/navigation.component';
-import { PeopleListComponent } from '../people/people-list/people-list.component';
-import { PlanetsListComponent } from '../planets/planets-list/planets-list.component';
-
-const routes: Routes = [
-  { path: 'people', component: PeopleListComponent },
-  { path: 'planets', component: PlanetsListComponent },
-  { path: '', redirectTo: '/people', pathMatch: 'full' },
-];
 
 @NgModule({
   declarations: [NavigationComponent],
-  imports: [RouterModule.forRoot(routes), CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavigationComponent],
 })
 export class CoreModule {}
